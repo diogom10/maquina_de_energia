a77e0c77aece261f6ef0926ebbc90f6622396671
diogo-houpa <diogo@houpa.app>
2018-08-29 20:00:35 -0300

    comentario

--- config/app.php
+++ config/app.php
@@ -15,6 +15,8 @@ return [
 
     'name' => env('APP_NAME', 'Laravel'),
 
+    //coment
+
     /*
     |--------------------------------------------------------------------------
     | Application Environment
